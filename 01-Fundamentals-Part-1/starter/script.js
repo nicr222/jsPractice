541d2c0d33c0b6a425649ea6484629114a71eccd
Nicolas Ross <15972995@mstc.edu>
2025-01-20 13:57:13 -0600

    Going through values, variables, and data types.

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,28 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10); 
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+let first = 'Jonas';
+let firstNamePerson = 'Nic';
+let first_name = "Nic";
+
+
+console.log(firstName);
+console.log(first);
+console.log(firstNamePerson);
+console.log(first_name);
+
+// Variable name conventions
+let nic_ross = 'NR';
+let $function = '27';
+
+let person = 'nic';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
