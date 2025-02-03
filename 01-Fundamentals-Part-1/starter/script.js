2cb6140e237645bec25776faa8a87de7e1f1d963
Nicolas Ross <15972995@mstc.edu>
2025-02-03 10:28:24 -0500

    fi

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -26,7 +26,6 @@ let myFirstJob = 'Programmer';
 let myCurrentJob = 'Teacher';
 
 console.log(myFirstJob);
-*/
 
 true;
 let javascriptIsFun = true;
@@ -57,3 +56,102 @@ console.log(age);
 age = 31;
 
 console.log(age);
+
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1992;
+// birthYear = 1990;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'ross';
+console.log(lastName);
+
+// Basic Operators
+const now = 2024;
+const ageNic = now - 1992;
+const ageSam = now - 1991;
+console.log(ageNic, ageSam);
+
+
+console.log(ageNic * 2, ageNic / 10, 2 ** 3);
+
+const firstName = 'Nic';
+const lastName = 'Ross';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageNic > ageSam); // >, <, >=, <=
+console.log(ageSam >= 18);
+
+const isFullAge = ageSam >= 18;
+
+console.log(isFullAge);
+
+// Basic Operators
+const now = 2024;
+const ageNic = now - 1992;
+const ageSam = now - 1991;
+console.log(now - 1992, now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+
+const favorite = prompt("What is your favorite number?");
+console.log(promppt(favorite));
+
+
+const day = 'friday';
+
+switch (day) {
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day');
+}
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend');
+} else {  
+    console.log('Not a valid day');
+}
+    */
